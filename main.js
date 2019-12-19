97410b4e4fef7eac287df358e07da511c37cb2a7
Doug Kintop <dkintop@hotmail.com>
2019-12-19 14:02:23 -0500

    Done.

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
+const headerError = document.querySelector("#modal");
+
+// Your JavaScript code goes here!
+
+document.addEventListener("DOMContentLoaded", event => {
+  console.log("DOM fully loaded and parsed");
+  hideError();
+});
+
+function hideError() {
+  headerError.classList.add("hidden");
+  console.log("hideError() called");
+}
+
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  red: "",
+  "": "red"
+};
+
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage) {
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      // Basic
+      // alert("Something went wrong!");
+      // or....
+      document.getElementById("modal").className = "";
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
