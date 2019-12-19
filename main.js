89bb7e99a44bb185b297b4f610eb5ae93d371ee6
Doug Kintop <dkintop@hotmail.com>
2019-12-19 13:31:59 -0500

    Done.

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
+const headerError = document.querySelector("#modal");
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", event => {
+  console.log("DOM fully loaded and parsed");
+  hideError();
+});
 
-
+function hideError() {
+  headerError.classList.add("hidden");
+  console.log("hideError() called");
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
