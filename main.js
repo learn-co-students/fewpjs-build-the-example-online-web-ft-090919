38b7f100fb07e484a4dcd7addc3d8bb4c294d8ff
Doug Kintop <dkintop@hotmail.com>
2019-12-19 13:56:05 -0500

    Done.

--- main.js
+++ main.js
@@ -15,7 +15,25 @@ function hideError() {
   console.log("hideError() called");
 }
 
-mimicServerCall();
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage) {
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      // Basic
+      // alert("Something went wrong!");
+      // or....
+      document.getElementById("modal").className = "";
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
