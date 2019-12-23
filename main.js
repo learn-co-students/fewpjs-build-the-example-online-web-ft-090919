8833b0a35d54fc06197f1a7935465d315b1c0916
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-23 13:31:26 -0500

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,53 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const HEART_STYLE = {
+  "like-glyph" : "like-glyph activated-heart",
+  "like-glyph activated-heart" : "like-glyph"
+}
+
+function hideError(){
+  let modal = document.querySelector("#modal")
+    modal.classList.add("hidden")
+    return true
+}
+
+function showError(error){
+  let modal = document.querySelector("#modal")
+  let message = document.querySelector("#modal-message")
+
+  message.textContent = error
+  modal.classList.remove("hidden")
+  setTimeout(function(){ hideError() }, 5000)
+}
 
 
+function likeHandler(e){
+  mimicServerCall()
+  .then(function(resp){
+    return resp.json()
+  })
+  .then(function(json){
+    let heartSpan = e.target
+    heartSpan.className = HEART_STYLE[heartSpan.className]
+  })
+  .catch(function(error){
+    let heartSpan = e.target
+    showError(error)
+  })
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  let likes = document.querySelectorAll(".like-glyph")
+
+  likes.forEach(elem => elem.addEventListener('click', function(e){
+    likeHandler(e)
+  }))
+
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
